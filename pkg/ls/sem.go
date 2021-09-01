b541e2763efb53e781ce58e1ce7eceb72182a23c
Burak Serdar <brxd@cloudprivacylabs.com>
2021-08-31 22:12:44 -0600

    Fix license headers

--- pkg/ls/sem.go
+++ pkg/ls/sem.go
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package ls
 
 // TermSemantics is used to describe how a term operates within the
