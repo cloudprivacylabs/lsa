6e16a63b2bc8aca4000c20ea389328c9de042406
Jason Cruz <jac024@ucsd.edu>
2022-02-04 20:46:14 -0800

    getschema

--- layers/cmd/getschema_csv.go
+++ layers/cmd/getschema_csv.go
@@ -27,7 +27,7 @@ import (
 
 func init() {
 	getschemaCmd.AddCommand(getschemaCSVCmd)
-	getschemaCSVCmd.Flags().Int("headerRow", -1, "Header row 0-based (default: no header)")
+	getschemaCSVCmd.Flags().Int("headerRow", 0, "Header row 0-based (default: 1st row)")
 }
 
 // go run main.go getschema csv ../examples/ghp/GHP_data_capture_global-en-ca.csv
@@ -49,18 +49,121 @@ var getschemaCSVCmd = &cobra.Command{
 		if err != nil {
 			failErr(err)
 		}
-		for row := 0; row < headerRow; row++ {
+		for row := 0; row <= headerRow; row++ {
 			rowData, err := reader.Read()
 			if err == io.EOF {
 				break
 			}
 			if err != nil {
 				failErr(err)
+			} else if row < headerRow {
+				continue
 			} else {
-				templateData := map[string]interface{}{
-					"columns": rowData,
+				// var jsObj map[string]interface{}
+				// for i := range rowData {
+				// 	jsObj = map[string]interface{}{
+				// 		"@id":           rowData[i],
+				// 		"@type":         "Value",
+				// 		"attributeName": rowData[i],
+				// 	}
+				// }
+				// attributes := []interface{}{}
+				// for _, hdr := range jsObj {
+				// 	attributes = append(attributes, hdr)
+				// }
+				// attributeList := make(map[string]interface{})
+				// attributeList["attributesList"] = attributes
+				// layer := make(map[string]interface{})
+				// layer["layer"] = attributeList
+				// var LS struct {
+				// 	Layer struct {
+				// 		AttributeList []struct {
+				// 			ID            string `json:"@id"`
+				// 			AttributeName string `json:"attributeName"`
+				// 			Type          string `json:"@type"`
+				// 		} `json:"attributeList"`
+				// 	} `json:"layer"`
+				// }
+
+				type AttributeList struct {
+					Id            string `json:"@id"`
+					AttributeName string `json:"attributeName"`
+					Types         string `json:"@type"`
 				}
-				js, err := json.MarshalIndent(templateData, "", "\t")
+
+				type Layer struct {
+					AttributeList []AttributeList `json:"attributeList"`
+				}
+
+				type LS struct {
+					Layer Layer `json:"layer"`
+				}
+
+				attHeaders := []AttributeList{}
+				for i := range rowData {
+					attHeaders = append(attHeaders, AttributeList{
+						Id:            rowData[i],
+						AttributeName: rowData[i],
+						Types:         "Value",
+					})
+				}
+
+				test := LS{
+					Layer: Layer{
+						AttributeList: attHeaders,
+					},
+				}
+
+				// for i := range rowData {
+				// }
+				// marshal csv to json, unmarshal json to LS struct
+				js, err := json.Marshal(rowData)
+				if err != nil {
+					failErr(err)
+				}
+
+				json.Unmarshal(js, &test)
+				//buffer := &bytes.Buffer{}
+				//fmt.Println(test)
+
+				// gob.NewEncoder(buffer).Encode(rowData[0])
+				// byteSlice := buffer.Bytes()
+				// fmt.Println(string(byteSlice))
+				//json.Unmarshal(byteSlice, &LS)
+
+				// type Inner struct {
+				// 	Object map[string]interface{} `json:"attributes"`
+				// 	// AttributeName map[string]interface{} `json:"@attributeName"`
+				// 	// Type          map[string]interface{} `json:"@type"`
+				// }
+				// type Outer struct {
+				// 	AttributeList []Inner `json:"attributeList"`
+				// }
+				// type Outmost struct {
+				// 	Layer Outer `json:"layer"`
+				// }
+				// var cont Outmost
+				// templateData := map[string]interface{}{
+				// 	"layer": map[string]interface{}{
+				// 		"attributeList": map[string]interface{}{
+				// 			"@id":           rowData,
+				// 			"attributeName": rowData,
+				// 			"Value":         rowData,
+				// 		},
+				// 	},
+				// }
+
+				//json.Unmarshal(byteSlice, &LS)
+
+				// cont.Layer.AttributeList = append(cont.Layer.AttributeList, Inner{Object: map[string]interface{}{
+				// 	"@id":            rowData[0],
+				// 	"@attributeName": rowData[0],
+				// 	"@type":          rowData[0],
+				// }})
+
+				// json.Unmarshal(byteSlice, &cont)
+
+				js, err = json.MarshalIndent(test, "", "\t")
 				if err != nil {
 					failErr(err)
 				}
