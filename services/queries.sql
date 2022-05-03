c237a1e94783f38672ea5959325936e8d925f44d
Jason Cruz <jac024@ucsd.edu>
2022-05-03 15:38:52 -0700

    valueset pipeline, python web service

--- services/queries.sql
+++ services/queries.sql
@@ -1,4 +1,5 @@
-gender_query = "SELECT {field} FROM {table} WHERE {pkey} = %s").format(
+gender_query = 
+        "SELECT {field} FROM {table} WHERE {pkey} = %s").format(
         field=sql.Identifier('my_name'),
-        table=sql.Identifier('some_table'),
+        table=sql.Identifier('concept'),
         pkey=sql.Identifier('id'))
