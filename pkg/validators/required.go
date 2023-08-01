09d779ddcb35e3c319656fe8970a7aa3271f3e92
Burak Serdar <brxd@cloudprivacylabs.com>
2023-08-01 10:26:21 -0600

    Required refactor

--- pkg/validators/required.go
+++ pkg/validators/required.go
@@ -1,6 +1,8 @@
 package validators
 
 import (
+	"fmt"
+
 	"github.com/cloudprivacylabs/lpg/v2"
 	"github.com/cloudprivacylabs/lsa/pkg/ls"
 )
@@ -11,18 +13,29 @@ import (
 //	  @id: attrId
 //	  validation/required: true
 //	}
-var RequiredTerm = ls.RegisterBooleanTerm(ls.NewTerm(ls.LS, "validation/required").SetComposition(ls.OverrideComposition).SetTags(ls.ValidationTag, ls.SchemaElementTag).SetMetadata(struct {
+var RequiredTerm = ls.NewTerm(ls.LS, "validation/required").SetComposition(ls.OverrideComposition).SetTags(ls.ValidationTag, ls.SchemaElementTag).SetMetadata(struct {
 	RequiredValidator
 }{
 	RequiredValidator{},
-}))
+}).Register()
 
 // RequiredValidator validates if a required value exists
 type RequiredValidator struct{}
 
+func requiredTermBool(node *lpg.Node) bool {
+	pv, ok := ls.GetPropertyValue(node, RequiredTerm.Name)
+	if !ok {
+		return false
+	}
+	bl := ls.BooleanType{}
+	s, _ := bl.Coerce(pv.Value())
+	v, _ := s.(bool)
+	return v
+}
+
 // ValidateValue checks if value is nil. If value is nil and it is required, returns an error
 func (validator RequiredValidator) ValidateValue(value *string, schemaNode *lpg.Node) error {
-	if RequiredTerm.PropertyValue(schemaNode) && value == nil {
+	if requiredTermBool(schemaNode) && value == nil {
 		return ls.ErrValidation{Validator: RequiredTerm.Name, Msg: "Missing required attribute: " + ls.GetNodeID(schemaNode)}
 	}
 	return nil
@@ -30,7 +43,7 @@ func (validator RequiredValidator) ValidateValue(value *string, schemaNode *lpg.
 
 // ValidateNode checks if value is nil. If value is nil and it is required, returns an error
 func (validator RequiredValidator) ValidateNode(docNode, schemaNode *lpg.Node) error {
-	if RequiredTerm.PropertyValue(schemaNode) {
+	if requiredTermBool(schemaNode) {
 		if docNode == nil {
 			return ls.ErrValidation{Validator: RequiredTerm.Name, Msg: "Missing required attribute: " + ls.GetNodeID(schemaNode)}
 		}
@@ -38,6 +51,58 @@ func (validator RequiredValidator) ValidateNode(docNode, schemaNode *lpg.Node) e
 		if !ok {
 			return ls.ErrValidation{Validator: RequiredTerm.Name, Msg: "Missing required attribute: " + ls.GetNodeID(schemaNode)}
 		}
+	}
+	return nil
+}
+
+// Validate checks if value is nil. If value is nil and it is required, returns an error
+func (validator RequiredValidator) Validate(docNode, schemaNode *lpg.Node) error {
+	if docNode == nil {
+		return nil
+	}
+	sl := ls.StringSliceType{}
+	pv, ok := ls.GetPropertyValue(schemaNode, RequiredTerm.Name)
+	if !ok {
+		return nil
+	}
+	slice, err := sl.Coerce(pv.Value())
+	if err != nil {
+		return nil
+	}
+	required := slice.([]string)
+	if len(required) > 0 {
+		req := make(map[string]struct{})
+		for _, x := range required {
+			req[x] = struct{}{}
+		}
+		for edges := docNode.GetEdges(lpg.OutgoingEdge); edges.Next(); {
+			to := edges.Edge().GetTo()
+			if !to.GetLabels().Has(ls.DocumentNodeTerm.Name) {
+				continue
+			}
+			id := ls.SchemaNodeIDTerm.PropertyValue(to)
+			if len(id) > 0 {
+				delete(req, id)
+			}
+		}
+		if len(req) > 0 {
+			return ls.ErrValidation{Validator: RequiredTerm.Name, Msg: "Missing required attribute: " + fmt.Sprint(req)}
+		}
+	}
+	return nil
+}
+
+// CompileTerm compiles the required properties array
+func (validator RequiredValidator) CompileTerm(_ *ls.CompileContext, target ls.CompilablePropertyContainer, term string, value ls.PropertyValue) error {
+	if _, ok := value.Value().(string); ok {
+		return nil
+	}
+	if _, ok := value.Value().(bool); ok {
+		return nil
+	}
+	sl := ls.StringSliceType{}
+	if _, err := sl.Coerce(value.Value()); err != nil {
+		return ls.ErrValidatorCompile{Validator: RequiredTerm.Name, Object: target, Msg: "Array of required attributes expected"}
 	}
 	return nil
 }
